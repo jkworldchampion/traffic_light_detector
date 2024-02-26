eca93d4ee4b73804db0dcbe5ddb3944950959f76
Dongwon-tuna <61178312+Dongwon-tuna@users.noreply.github.com>
2024-02-26 15:24:11 +0900

    add hello

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ultralytics import YOLO
 
 # Load the model
 model = YOLO("yolov8n.yaml")
+print("hello")
 
 # Use the model
 results = model.train(data="D:/programming/book/traffic_light_detector/config.yaml", epochs=5) 
